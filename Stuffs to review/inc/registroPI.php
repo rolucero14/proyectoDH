83b36eaa6eb9ab18f68a950510baa9c85a3c62fb
Rodrigo <rolucero14@gmail.com>
2019-06-21 12:46:27 -0300

    organize
    Giving some order to repo

---
+++ Stuffs to review/inc/registroPI.php
@@ -0,0 +1,95 @@
+<?php
+
+$provincias = [
+  "s"=>"San Luis",
+  "m"=>"Mendoza",
+  "j"=>"San Juan",
+  "n"=>"Neuquen"
+];
+
+  if($_POST){
+    if(empty($_POST["nombre"])){
+    echo "Falta ingresar datos de Nombre <br>";
+    }
+    // strlend es una función que nos ayuda a contar variables dentro de los datos a completar en este caso es el valor 0, si no se completa envia el dato "Falta ingresar datos de Nombre" //
+
+    if(strlen($_POST["usuario"]) <8 ){
+  echo "El usuario debe tener al menos 8 caracteres <br>";
+    }
+
+    if(is_numeric($_POST["edad"]) == false){
+  echo "La edad debe ser numerica <br>";
+    }
+
+    if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL==false)){
+  echo "El mail no tiene formato correspondiente <br>";
+}}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+  <body>
+    <form class="" action="registroPI.php" method="POST">
+
+      <div class="">
+        <label for=""> Nombre:</label>
+        <input type="text" name="nombre" value="">
+      </div>
+
+      <div class="">
+        <label for=""> Usuario:</label>
+        <input type="text" name="usuario" value="">
+      </div>
+
+      <div class="">
+        <label for=""> Email:</label>
+        <input type="text" name="email" value="">
+      </div>
+
+      <div class="">
+        <label for=""> Repetir Email:</label>
+        <input type="text" name="email" value="">
+      </div>
+
+      <div class="">
+        <label for=""> Edad:</label>
+        <input type="text" name="edad" value="">
+      </div>
+
+      <div class="">
+        <label for=""> Password:</label>
+        <input type="password" name="password" value="">
+      </div>
+
+      <div class="">
+        <label for="">Repetir Password:</label>
+        <input type="password" name="password" value="">
+      </div>
+
+      <div class="">
+        <label for=""> Telefono:</label>
+        <input type="text" name="Telefono" value="">
+      </div>
+
+      <div  class="">
+        <label for =""> Provincia: </label>
+        <select class="" name="provincia">
+        <?php foreach ($provincias as $codigo => $provincia): ?>
+            <option value="<?=$codigo?>">
+          <?=$provincia?>
+        </option>
+      <?php endforeach ?>
+    </select>
+      </div>
+
+      <div class="">
+        <input type="submit" name="submit" value="enviar">
+      </div>
+
+    </form>
+  </body>
---
+++ Stuffs to review/products.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>PRODUCTOS</title>
+  </head>
+  <body>
+    <header>
+      <div class="p-3 mb-2 bg-primary text-white"><h1>Contratá servicios para tu casa o empresa</h1>
+      <img src="imagenes/LOGO_HERALDO.png" width="" width="400">
+
+    </header>
+    <main>
+      <h1>SERVICIOS</h1>
+      <section class="oficios">
+          <div class="container">
+            <h2>Gas</h2>
+            <img src="imagenes/gasista.jpg" class="" width="250">
+            <ul style="list-style-type: disc">
+              <li>Instalaciones</li>
+              <li>Reparaciones</li>
+              <li>Escapes de gas</li>
+              <li>Normativas</li>
+              <button type="button" class="btn btn-primary btn-lg btn-block">Solicita tu presupuesto</button>
+            </ul>
+          </div>
+          <div class="container">
+            <h2>Plomería</h2>
+            <img src="imagenes/plomero.jpg" class="" width="250">>
+            <ul style="list-style-type: disc">
+              <li>Instalaciones</li>
+              <li>Reparaciones</li>
+              <li>Fugas o perdidas</li>
+              <li>Redes, cloacas, pluviales</li>
+              <button type="button" class="btn btn-primary btn-lg btn-block">Solicita tu presupuesto</button>
+            </ul>
+          </div>
+          <div class="container" >
+            <h2>Albañilería</h2>
+            <img src="imagenes/albañil.jpg
+            " class="" width="250">>
+            <ul style="list-style-type: disc">
+              <li>Obra gruesa</li>
+              <li>Reparaciones</li>
+              <li>Terminaciones</li>
+              <li>Proyectos totales</li>
+              <button type="button" class="btn btn-primary btn-lg btn-block">Solicita tu presupuesto</button>
+            </ul>
+          </div>
+          <div class="container" >
+            <h2>Pintura</h2>
+            <img src="imagenes/pintor.jpg" class="" width="250">>
+            <ul style="list-style-type: disc">
+              <li>Exterior</li>
+              <li>Interior</li>
+              <li>Airless y tradicional</li>
+              <li>Grandes superficies</li>
+              <button type="button" class="btn btn-primary btn-lg btn-block">Solicita tu presupuesto</button>
+            </ul>
+          </div>
+          <div class="container" >
+            <h2>Electrodomésticos</h2>
+            <img src="imagenes/electrodomestico ii.jpg" class="" width="250">>
+            <ul style="list-style-type: disc">
+              <li>Reparaciones</li>
+              <li>Audio, Video, Computación</li>
+              <li>Electro de Cocina</li>
+              <li>Lavadoras y Secadoras</li>
+              <button type="button" class="btn btn-primary btn-lg btn-block">Solicita tu presupuesto</button>
+            </ul>
+          </div>
+          <div class="container" >
+            <h2>Carpintería</h2>
+            <img src="imagenes/Carpinteria.jpg" class="" width="250">>
+            <ul style="list-style-type: disc">
+              <li>Aluminio, PVC y Madera</li>
+              <li>Aislación térmica y acústica</li>
+              <li>Instalación</li>
+              <li>Fabricación</li>
+              <button type="button" class="btn btn-primary btn-lg btn-block">Solicita tu presupuesto</button>
+            </ul>
+          </div>
+          <div class="container" >
+            <h2>Electricidad</h2>
+            <img src="imagenes/Electricista.jpg" class="" width="250">>
+            <ul style="list-style-type disc">
+              <li>Instalaciones</li>
+              <li>Reparaciones</li>
+              <li>Mdificaciones</li>
+              <li>Grandes obras</li>
+              <button type="button" class="btn btn-primary btn-lg btn-block">Solicita tu presupuesto</button>
+            </ul>
+          </div>
+          <div class="container" >
+            <h2>Cerrajería</h2>
+              <img src="imagenes/key.jpg" class=""  width="250">
+              <ul style="list-style-type: disc">
+                <li>Instalaciones</li>
+                <li>Reparaciones</li>
+                <li>Automatización</li>
+                <li>Codificación</li>
+                <button type="button" class="btn btn-primary btn-lg btn-block">Solicita tu presupuesto</button>
+              </ul>
+          </div>
+          <div class="container" >
+            <h2>Refrigeración</h2>
+            <img src="imagenes/aireAcondicionad.jpg" class="" width="250">
+            <ul style="list-style-type: disc">
+              <li>Instalaciones</li>
+              <li>Reparaciones</li>
+              <li>Escapes de gas</li>
+              <li>Normativas</li>
+              <button type="button" class="btn btn-primary btn-lg btn-block">Solicita tu presupuesto</button>
+            </ul>
+          </div>
+       </section>
+    </main>
+    <footer>
+
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
